735dc5d1cce1b1bcbbab09e4fab4e1015caea19a
Claudio <soarescjunior>
2022-03-20 15:04:54 -0300

    Exercicio05 Easy - refatoração

--- Exercicios_Turma4/src/easy/Exercicio05.java
+++ Exercicios_Turma4/src/easy/Exercicio05.java
@@ -6,20 +6,17 @@ public class Exercicio05 {
     //armazenar em variáveis, e exibir a soma, a subtração, 
     //multiplicação e a média dos três números digitados.
 
-    public void calculaOperracoesAritimeticas( int numeroUm, int numeroDois, int numeroTres) {
+    public String calculaOperracoesAritimeticas( int numeroUm, int numeroDois, int numeroTres) {
         
         int soma, subtração, divisao, multiplicacao;
         
-
         soma = numeroUm + numeroDois + numeroTres;
         subtração = numeroUm - numeroDois - numeroTres;
         multiplicacao = numeroUm * numeroDois * numeroTres;
         divisao = soma / 3;
 
-        System.out.println("A soma é: " + soma);
-        System.out.println("A subtração é:  " + subtração);
-        System.out.println("A multiplicação: " + multiplicacao);
-        System.out.println("A divisao é:  " + divisao);
+        return "A soma é: " + soma + "\n" + "A subtração é: " + subtração 
+        + "\n" + "A multiplicação: " + multiplicacao + "\n" + "A divisao é: " + divisao ;
     
     }
     
--- Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio05_easy.java
+++ Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio05_easy.java
@@ -1,19 +1,14 @@
 package execucao_Exercicios_Easy;
 
-import javax.swing.JOptionPane;
-
 import easy.Exercicio05;
 
 public class Execucao_Exercicio05_easy {
     
     public static void main(String[] args) {
 
-        int numeroUm = Integer.parseInt(JOptionPane.showInputDialog("Digite o numero um: "));
-        int numeroDois =  Integer.parseInt(JOptionPane.showInputDialog("Digite o numero dois: "));
-        int numeroTres =  Integer.parseInt(JOptionPane.showInputDialog("Digite o numero tres: "));
-
-        Exercicio05 ex5 = new Exercicio05();
-        ex5.calculaOperracoesAritimeticas(numeroUm, numeroDois, numeroTres);        
+                Exercicio05 executarExercicio05 = new Exercicio05();
+               System.out.println(executarExercicio05.calculaOperracoesAritimeticas(5, 2, 5));
+               
 
     }
 
