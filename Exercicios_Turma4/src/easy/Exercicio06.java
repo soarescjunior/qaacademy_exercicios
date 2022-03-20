524e458ab5ec6105b9e7141eeef6d32030e0cce3
Claudio <soarescjunior>
2022-03-20 20:41:19 -0300

    Exercicio06 Easy - refatoração

--- Exercicios_Turma4/src/easy/Exercicio06.java
+++ Exercicios_Turma4/src/easy/Exercicio06.java
@@ -6,19 +6,19 @@ public class Exercicio06 {
     // E SE - a média > 5 Exibir aprovado, SE a média < 5 exibir reprovado, se media
     // = 5 exibir exame.
 
-    public String calcularMediaAluno(double numeroUm, double numeroDois) { 
+    public String calcularMediaAluno(double notaUm, double notaDois) { 
         double media;
-            media = (numeroUm + numeroDois) / 2;
+            media = (notaUm + notaDois) / 2;
         if (media > 5.0) {
-            return ("Voce foi aprovado " + "sua media " + media);
+            return ("Voce foi aprovado " + "sua media é: " + media);
         } else
             ;
         if (media < 5.0) {
-            return("Voce foi reprovado " + "sua media é ") + media;
+            return("Voce foi reprovado " + "sua media é: ") + media;
         } else
             ;
         if (media == 5.0) {
-            return("Voce ficou de exame " + "sua media é " + media);
+            return("Voce ficou de exame " + "sua media é: " + media);
         } else
             ;
         return "Nota digitada invalida";
--- Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio06_easy.java
+++ Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio06_easy.java
@@ -1,18 +1,14 @@
 package execucao_Exercicios_Easy;
 
-import javax.swing.JOptionPane;
-
 import easy.Exercicio06;
 
 public class Execucao_Exercicio06_easy {
 
     public static void main(String[] args) {
 
-        double numeroUm = Double.parseDouble(JOptionPane.showInputDialog("Digite sua primeira nota: "));
-        double numeroDois = Double.parseDouble(JOptionPane.showInputDialog("Digite sua segunda nota: "));       
-
-        Exercicio06 ex6 = new Exercicio06();
-        System.out.println(ex6.calcularMediaAluno(numeroUm, numeroDois)); 
+        Exercicio06 executarExercicio06 = new Exercicio06();
+        System.out.println(executarExercicio06.calcularMediaAluno(5.5, 6.0));
+        
 
     }
     
