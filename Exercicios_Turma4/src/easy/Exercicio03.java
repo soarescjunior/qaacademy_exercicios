0a6516626904e8a2c873acc08b05237dbd76202b
Claudio <soarescjunior>
2022-03-20 11:59:45 -0300

    Exercicio03 Easy - refatoração

--- Exercicios_Turma4/src/easy/Exercicio03.java
+++ Exercicios_Turma4/src/easy/Exercicio03.java
@@ -1,19 +1,12 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio03 {
 
     //Exercício 3 - Faça um algoritmo para ler dois valores,
     //armazenar em variáveis, e exibir os valores das variáveis, trocados.
 
-    public void lerDoisValoresExibirValoresTrocados() {
-
-    String numeroum, numerodois;
-        numeroum = JOptionPane.showInputDialog("Digite o primeiro numero: ");
-        numerodois = JOptionPane.showInputDialog("Digite o segundo numero: ");
-        System.out.println("numero um é " + numerodois + " e " + "numero dois é: " + numeroum);
-
+    public String lerDoisValoresExibirValoresTrocados(int numeroUM, int numeroDois) {
+        return "numero um é " + numeroDois + " e " + "numero dois é: " + numeroUM;
     }
     
 }
--- Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio03_easy.java
+++ Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio03_easy.java
@@ -4,13 +4,11 @@ import easy.Exercicio03;
 
 public class Execucao_Exercicio03_easy {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Exercicio03 ex3 = new Exercicio03();
-
-        ex3.lerDoisValoresExibirValoresTrocados();
-
+        Exercicio03 executarExercicio03 = new Exercicio03();
+      System.out.println(executarExercicio03.lerDoisValoresExibirValoresTrocados(10, 20));
 
     }
-    
+
 }
