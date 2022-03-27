3bc3791107b818ad78936ae1cb9bbb62e81bd062
Claudio <soarescjunior>
2022-03-27 13:29:00 -0300

    Exercicio08 Easy - refatoração

--- Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio08_easy.java
+++ Exercicios_Turma4/src/execucao_Exercicios_Easy/Execucao_Exercicio08_easy.java
@@ -7,9 +7,9 @@ public class Execucao_Exercicio08_easy {
     public static void main(String[] args) {
     
        
-        Exercicio08 ex8 = new Exercicio08();
+        Exercicio08 executarExercicio08 = new Exercicio08();
 
-        ex8.calculoSalarioLiquidoEImposto();
+        System.out.println(executarExercicio08.calculoSalarioLiquidoEImposto(5000.68));
     
         }
     
