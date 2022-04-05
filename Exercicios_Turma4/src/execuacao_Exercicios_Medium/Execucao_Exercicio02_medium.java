7bee2a70955739f522a00d8b8bfb53097431ec0e
Claudio <soarescjunior>
2022-04-05 19:50:41 -0300

    Exercicio02 Medium - refatoração

---
+++ Exercicios_Turma4/src/execuacao_Exercicios_Medium/Execucao_Exercicio02_medium.java
@@ -0,0 +1,12 @@
+package execuacao_Exercicios_Medium;
+import medium.Exercicio02;
+
+public class Execucao_Exercicio02_medium  {
+
+    public static void main(String[] args) {
+
+        Exercicio02 executarExercicio02 = new Exercicio02();
+        System.out.println(executarExercicio02.valorInvestido(1000.00));
+    }
+  
+}
