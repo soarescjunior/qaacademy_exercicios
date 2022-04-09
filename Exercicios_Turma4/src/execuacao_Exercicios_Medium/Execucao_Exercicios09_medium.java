3a1d5e8828d7e41f11d08a97e808694d7c8cde21
Claudio <soarescjunior>
2022-04-09 15:12:56 -0300

    Exercicio09 Medium - refatoração

---
+++ Exercicios_Turma4/src/execuacao_Exercicios_Medium/Execucao_Exercicios09_medium.java
@@ -0,0 +1,17 @@
+package execuacao_Exercicios_Medium;
+import medium.Exercicio07;
+import medium.Exercicio09;
+
+public class Execucao_Exercicios09_medium {
+
+    public static void main(String[] args) {
+
+    String [] vetorAlunos = {"Flavio", "Reinaldo", "Samuel", "Edmond", "Pedro"};
+    int [] vetorNumeroAlunos = {1,2,3,4,5};
+
+    Exercicio09 executarExercicio09 = new Exercicio09();
+    executarExercicio09.exibeAlunosENumeros(vetorAlunos, vetorNumeroAlunos);
+
+    }
+
+}
